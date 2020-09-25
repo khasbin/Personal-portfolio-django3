dc3a18d5e80964ed8f76a91afc1dfc0cbd1443b9
khasbin@gmail.com <khasbin@gmail.com>
2020-09-25 11:38:23 +0545

    Simple changes in the blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class blog(models.Model):
+    title = models.CharField(max_length=150)
+    date = models.DateField()
+    description = models.TextField(max_length = 5000)
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import blog
 # Create your views here.
+
+def all_blogs(request):
+    context = blog.objects.order_by('-date')[:3]
+    return render(request, 'blog/blog.html',{'contexts':context})
